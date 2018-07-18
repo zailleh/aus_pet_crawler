103bd0df7ebb3f5af3fc6ab8cf2096075992c993
Tim Caldwell <tim.caldwell@einti.com>
2018-07-18 16:32:33 +1000

    add api for pet images

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,11 @@ class ApiController < ApplicationController
 
   def pets
     pets = Pet.all
-    render :plain => pets.to_json
+    render :plain => pets.to_json, content_type: "application/json"
+  end
+
+  def photos
+    photos = Photo.where :pet_id => params[:id]
+    render :plain => photos.to_json, content_type: "application/json"
   end
 end
