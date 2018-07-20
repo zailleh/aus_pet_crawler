c64fa6fe40756ab9667c2664f8acbbd7f12a74ff
Tim Caldwell <tim.caldwell@einti.com>
2018-07-20 10:17:45 +1000

    simplify routes and api controller to return json defaults

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,23 +1,19 @@
 class ApiController < ApplicationController
 
   def pets
-    pets = Pet.all
-    render :plain => pets.to_json, content_type: "application/json"
+    render :json => Pet.all
   end
 
   def photos
-    photos = Photo.where :pet_id => params[:id]
-    render :plain => photos.to_json, content_type: "application/json"
+    render :json => Photo.where(:pet_id => params[:id])
   end
 
   def shelter
-    shelter = Shelter.find_by :shelter_id => params[:id]
-    render :plain => shelter.to_json, content_type: "application/json"
+    render :json => Shelter.find_by(:shelter_id => params[:id])
   end
 
   def shelters
-    shelters = Shelter.all
-    render :plain => shelters.to_json, content_type: "application/json"
+    render :json => Shelter.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "api#pets"
-  get '/pets' => "api#pets"
-  get '/pets/:id' => "api#photos"
-  get '/shelters' => "api#shelters"
-  get '/shelters/:id' => "api#shelter"
+  get '/pets' => "api#pets", defaults: { format: :json }
+  get '/pets/:id' => "api#photos", defaults: { format: :json }
+  get '/shelters' => "api#shelters", defaults: { format: :json }
+  get '/shelters/:id' => "api#shelter", defaults: { format: :json }
 end
